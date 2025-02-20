f2f1d4bcbbfbc4350a561973e342dbcfc9785812
n3tw4rl0k <120563953+n3tw4rl0k@users.noreply.github.com>
2025-02-20 15:03:11 +0200

    solution to write-a-function (leap year) problem

---
+++ Python/medium/write-a-function.py
@@ -0,0 +1,37 @@
+import sys
+from io import StringIO
+
+orig_stdin = sys.stdin
+
+# test case 1
+# input_sample = "5"
+
+# test case 2
+input_sample = "2100"
+
+sys.stdin = StringIO(input_sample)
+
+
+def is_leap(year):
+    leap = False
+    lock_found = False
+
+    div_by_4 = year % 4
+    div_by_100 = year % 100
+    div_by_400 = year % 400
+    if div_by_100 == 0 and div_by_400 == 0:
+        leap = True
+        lock_found = True
+    if div_by_100 == 0 and not lock_found:
+        leap = False
+        lock_found = True
+    if div_by_4 == 0 and not lock_found:
+        leap = True
+
+    return leap
+
+
+year = int(input())
+print(is_leap(year))
+
+sys.stdin = orig_stdin
