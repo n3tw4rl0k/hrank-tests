2f7f0856c3925ad71917cdaa7bd720593bb8a7df
n3tw4rl0k <120563953+n3tw4rl0k@users.noreply.github.com>
2025-02-23 12:21:18 +0200

    solution to for loop in c

---
+++ C-programming/Conditionals and loops/for-loop-in-c.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    int c = b;
+    if(b > 9){
+        c = 9;
+    }
+    for(int i = a; i <= c; i++) {
+        if (i == 1){
+            printf("one\n");
+        } else if (i == 2){
+            printf("two\n");
+        } else if (i == 3){
+            printf("three\n");
+        } else if (i == 4){
+            printf("four\n");
+        } else if (i == 5){
+            printf("five\n");
+        } else if (i == 6){
+            printf("six\n");
+        } else if (i == 7){
+            printf("seven\n");
+        } else if (i == 8){
+            printf("eight\n");
+        } else if (i == 9){
+            printf("nine\n");
+        }
+    }
+
+    for(int i = 10; i <= b; i++) {
+        if (i % 2 == 0) {
+            printf("even\n", i);
+        } else {
+            printf("odd\n", i);
+        }
+    }
+
+    return 0;
+}
